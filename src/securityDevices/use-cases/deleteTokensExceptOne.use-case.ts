915063aeadc9cbc7d93b8b34d343c27f441ccf66
OtherOl <pilyak003@gmail.com>
2024-02-01 17:50:50 +0300

    fix

--- src/securityDevices/use-cases/deleteTokensExceptOne.use-case.ts
+++ src/securityDevices/use-cases/deleteTokensExceptOne.use-case.ts
@@ -15,7 +15,7 @@ export class DeleteTokensExceptOneUseCase {
     const decodedToken = await this.authService.decodeRefreshToken(refreshToken);
     const tokens = await this.authWhileListRepository.findTokens(decodedToken.userId, decodedToken.deviceId);
     for (let i = 0; i < tokens.length; i++) {
-      await this.authBlackListRepository.blackList(tokens[i]);
+      await this.authBlackListRepository.blackList(tokens[i].token);
     }
     await this.authWhileListRepository.deleteAllExceptOne(decodedToken.userId, decodedToken.deviceId);
     return;
